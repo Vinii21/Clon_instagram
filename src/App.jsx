7b3051acc4b55c14997d27dacc2906a610e7b397
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-05-16 09:56:36 -0600

    Inicio del proyecto, estructuracion de carpetas, primer componente

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.page'
+
+function App() {
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />
+      }/>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+const Search = () => {
+    return (
+        <div>
+            <h1 style={{display: "none"}}>Instagram</h1>
+            <div>
+                <i className='bx bx-search'></i>
+                <input type="text" />
+            </div>
+            <i className='bx bx-heart'></i>
+        </div>
+    );
+}
+ 
+export default Search;
